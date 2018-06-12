3312e80bf3ddcdec8d13dbdd49eb8b3a8ecaad6b
Dozono <dozono@dozono.net>
2018-06-13 01:01:40 +0900

    Create Repository

---
+++ kadai3.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## exit code
+# 0 : 入力値は自然数です。
+# 1 : 入力値は自然数ではありません。
+
+## main
+### Input value
+INPUT=${1}
+
+### Check prime
+if [ ${INPUT} -lt 2 ] ; then
+  echo "入力値は素数ではありません。"
+  exit 1
+elif [ ${INPUT} == 2 ] ; then
+  echo "入力値は素数です。"
+  exit 0
+else
+  I=2
+  while [ ${I} -lt ${INPUT} ] ; do
+    CHECKNUM=$((${INPUT} % ${I}))
+    if [ ${CHECKNUM} -eq 0 ] ; then
+      echo "入力値は素数ではありません。"
+      exit 0
+    fi
+    ((I++))
+  done
+  echo "入力値は素数です。"
+  exit 1
+fi

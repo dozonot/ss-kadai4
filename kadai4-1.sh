83cd019feae50c9c6a85dab82a89a6163a788b71
Dozono <dozono@dozono.net>
2018-06-13 14:34:51 +0900

    modify kadai4-1.sh ./ to sh

--- kadai4-1.sh
+++ kadai4-1.sh
@@ -12,7 +12,8 @@ ERROR_EXIT () {
 
 # テスト開始
 # test1: 2:素数
-./kadai3.sh 3 > ${TMP}-ans && ERROR_EXIT "error in test1-1"
+sh kadai3.sh 3 > ${TMP}-ans && ERROR_EXIT "error in test1-1"
 diff ${TMP}-ans ${TMP}-true || ERROR_EXIT "error in test1-2"
 
 rm -f ${TMP}-*
+exit 0

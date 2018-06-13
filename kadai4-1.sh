a525db56c7746d70c01c0c25aaf41a2aa4bc6537
Dozono <dozono@dozono.net>
2018-06-13 12:51:38 +0900

    modify kadai4-1.sh

--- kadai4-1.sh
+++ kadai4-1.sh
@@ -5,14 +5,14 @@ echo "入力値は素数です。"           > ${TMP}-true
 echo "入力値は素数ではありません。" > ${TMP}-false
 
 ERROR_EXIT () {
-  echo "$1" >^2
+  echo "$1" >&2
   rm -f ${TMP}-*
   exit 1
 }
 
 # テスト開始
 # test1: 2:素数
-./kadai3.sh 2 > ${TMP}-ans && ERROR_EXIT "error in test1-1"
+./kadai3.sh 3 > ${TMP}-ans && ERROR_EXIT "error in test1-1"
 diff ${TMP}-ans ${TMP}-true || ERROR_EXIT "error in test1-2"
 
 rm -f ${TMP}-*

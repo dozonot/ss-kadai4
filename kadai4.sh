86a820b0ab349c7902df117df86043b6f01ec4e5
Dozono <dozono@dozono.net>
2018-06-14 11:31:27 +0900

    Modify kadai4.sh, Add test case

--- kadai4.sh
+++ kadai4.sh
@@ -21,10 +21,15 @@ sh kadai3.sh 4 > ${TMP}-ans || ERROR_EXIT "error in test2-1"
 diff ${TMP}-ans ${TMP}-false || ERROR_EXIT "error in test2-2"
 echo "test2: 成功"
 
-echo "test3: 素数判定"
-sh kadai3.sh 5 > ${TMP}-ans || ERROR_EXIT "error in test3-1"
+echo "test3: 大きな素数判定"
+sh kadai3.sh 6700417 > ${TMP}-ans || ERROR_EXIT "error in test3-1"
 diff ${TMP}-ans ${TMP}-true || ERROR_EXIT "error in test3-2"
 echo "test3: 成功"
+
+echo "test4: 大きな非素数判定"
+sh kadai3.sh 6700417 > ${TMP}-ans || ERROR_EXIT "error in test4-1"
+diff ${TMP}-ans ${TMP}-true || ERROR_EXIT "error in test4-2"
+echo "test4: 成功"
 
 echo "一時ファイルの削除"
 rm -f ${TMP}-*

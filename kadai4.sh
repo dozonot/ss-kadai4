81edb5655bf16a41b339cda360de09b42b466abc
Dozono <dozono@dozono.net>
2018-06-14 11:27:29 +0900

    Modify kadai4.sh, add comment

--- kadai4.sh
+++ kadai4.sh
@@ -10,14 +10,15 @@ ERROR_EXIT () {
   exit 1
 }
 
-# テスト開始
-## test1: 素数
+echo "テスト開始"
+echo "test1: 素数判定"
 sh kadai3.sh 3 > ${TMP}-ans || ERROR_EXIT "error in test1-1"
 diff ${TMP}-ans ${TMP}-true || ERROR_EXIT "error in test1-2"
 
-## test2: 非素数
+echo "test2: 非素数判定"
 sh kadai3.sh 4 > ${TMP}-ans || ERROR_EXIT "error in test2-1"
 diff ${TMP}-ans ${TMP}-false || ERROR_EXIT "error in test2-2"
 
+echo "一時ファイルの削除"
 rm -f ${TMP}-*
 exit 0
